1ae63b26e14bce5acc3848850710f715eb73f0d2
AllenTeng-acer\AllenTeng <nsl.allenteng@gmail.com>
2017-07-25 09:59:50 +0800

    First version

---
+++ app/src/main/java/com/vitalsigns/demoholter/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vitalsigns.demoholter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
